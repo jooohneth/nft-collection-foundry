d47263727a8e017e51de10a40683b4749857767a
John <jooohn.eth@gmail.com>
2022-08-11 13:03:44 -0400

    Added test for max supply of the collection

--- test/NFTContract.t.sol
+++ test/NFTContract.t.sol
@@ -12,9 +12,26 @@ contract ContractTest is Test {
     address account2 = address(0x7777);
 
     function setUp() public {
+        //Setting the owner as the deployer of the contract
         vm.startPrank(owner);
         nftContract = new NFTContract();
         vm.stopPrank();
     }
+
+    function testFailMaxSupply() public {
+        //Setting account1 as the caller of the mint function
+        vm.startPrank(account1);
+        //Giving account1 a balance of 1.1 ether
+        vm.deal(account1, 1.1 ether);
+        //Looping and calling mint function 101 times => should lead to an error, since 100 is the max supply
+        for(uint i = 0; i < 101; i++){
+            nftContract.mint{value: nftContract.MINT_PRICE()}();
+        }
+        vm.stopPrank();
+
+    }
+
+    
+    
 
 }
